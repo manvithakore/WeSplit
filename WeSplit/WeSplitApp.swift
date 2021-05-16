ef6d798db5979bbfe6e8d657762da87df3bc067b
manvithakore <manvi.thakore@gmail.com>
2021-05-16 11:48:44 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Manvi Thakore on 2021-05-16.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
